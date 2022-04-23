6f0ec9a09a56f978aa2ebd8fa375fcc48dce4c72
Ankur Sharma <ankurs4@cisco.com>
2022-04-23 11:27:41 +0200

    done with selection sort and insertion sort

--- src-lib/api/sort_array_algorithms.hpp
+++ src-lib/api/sort_array_algorithms.hpp
@@ -11,12 +11,30 @@ void selection_sort(T (&arr)[N]);
 
 template<typename T, std::size_t N>
 void insertion_sort(T (&arr)[N]) {
-    (void) arr;
+    for (std::size_t i=1; i<N; i++) {
+        for (
+            std::size_t j = i;
+            j > 0 && arr[j] < arr[j-1];
+            j--
+        ) {
+            std::swap(arr[j], arr[j-1]);
+        }
+    }
 }
 
 template<typename T, std::size_t N>
 void selection_sort(T (&arr)[N]) {
-    (void) arr;
+    for (std::size_t i=0; i<N; i++) {
+        std::size_t smallest_index = i;
+        for (std::size_t j=i; j<N; j++) {
+            if (arr[j] < arr[smallest_index]) {
+                smallest_index = j;
+            }
+        }
+        if (smallest_index != i) {
+            std::swap(arr[i], arr[smallest_index]);
+        }
+    }
 }
 
 }
