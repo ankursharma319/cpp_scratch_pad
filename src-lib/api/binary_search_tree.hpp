9ec8eea55a6bef2a8cf80c44e1442d58c2466939
Ankur Sharma <ankurs4@cisco.com>
2022-04-24 14:13:34 +0200

    bst: contains, find_min, find_max, remove

--- src-lib/api/binary_search_tree.hpp
+++ src-lib/api/binary_search_tree.hpp
@@ -34,6 +34,25 @@ struct node {
             right_child->fill_string(ss, children_prefix + "|--", children_prefix + "   ");
         }
     }
+
+    void detach() {
+        left_child = nullptr;
+        right_child = nullptr;
+        parent = nullptr;
+    }
+    ~node() {
+        if (left_child) {
+            delete left_child;
+        }
+        if (right_child) {
+            delete right_child;
+        }
+        if (parent && parent->left_child == this) {
+            parent->left_child = nullptr;
+        } else if (parent && parent->right_child == this) {
+            parent->right_child = nullptr;
+        }
+    }
 };
 
 }
@@ -42,7 +61,11 @@ class binary_search_tree {
 public:
     binary_search_tree() : m_head(nullptr), m_size(0) {}
 
-    ~binary_search_tree() {}
+    ~binary_search_tree() {
+        if (m_head) {
+            delete m_head;
+        }
+    }
 
     bool contains(int value) const {
         return getNode(value) != nullptr;
@@ -90,7 +113,82 @@ public:
         return m_size;
     }
 
+    int find_min() const {
+        return find_min_node(m_head)->value;
+    }
+
+    int find_max() const {
+        return find_max_node(m_head)->value;
+    }
+
+    void remove(int value) {
+        remove_node_with_value(m_head, value);
+    }
+
 private:
+    bst_detail::node* find_min_node(bst_detail::node* root) const {
+        assert(root);
+        while (root->left_child) {
+            root = root->left_child;
+        }
+        return root;
+    }
+
+    bst_detail::node* find_max_node(bst_detail::node* root) const {
+        assert(root);
+        while (root->right_child) {
+            root = root->right_child;
+        }
+        return root;
+    }
+
+    bst_detail::node* delete_node(bst_detail::node * node) {
+        if (!node->left_child && !node->right_child) {
+            if (node->parent->left_child == node) {
+                node->parent->left_child = nullptr;
+            } else if(node->parent->right_child == node) {
+                node->parent->right_child = nullptr;
+            }
+            node->detach();
+            delete node;
+            m_size--;
+            return nullptr;
+        } else if(!node->left_child || !node->right_child) {
+            bst_detail::node* replacement_node = node->left_child;
+            if (!replacement_node) {
+                replacement_node = node->right_child;
+            }
+            if (node->parent->left_child == node) {
+                node->parent->left_child = replacement_node;
+            } else if(node->parent->right_child == node) {
+                node->parent->right_child = replacement_node;
+            }
+            replacement_node->parent = node->parent;
+            node->detach();
+            delete node;
+            m_size--;
+            return replacement_node;
+        } else {
+            // 2 children
+            bst_detail::node* replacement_node = find_min_node(node->right_child);
+            node->value = replacement_node->value;
+            return remove_node_with_value(node->right_child, replacement_node->value);
+        }
+    }
+
+    bst_detail::node* remove_node_with_value(bst_detail::node* root, int value) {
+        if(!root) {
+            return nullptr;
+        } else if (root->value > value) {
+            return remove_node_with_value(root->left_child, value);
+        } else if (root->value < value) {
+            return remove_node_with_value(root->right_child, value);
+        } else {
+            //found the node to be deleted
+            return delete_node(root);
+        }
+    }
+
     bst_detail::node* getNode(int value) const {
         bst_detail::node * node = m_head;
         while (node != nullptr) {
--- tests/BSTTests.cpp
+++ tests/BSTTests.cpp
@@ -6,8 +6,9 @@
 #include <numeric>
 #include <vector>
 
-TEST(BSTTest, test_insert_and_print) {
+TEST(BSTTest, test_size_insert_and_print) {
     ankur::binary_search_tree my_tree {};
+    EXPECT_EQ(my_tree.size(), 0);
     my_tree.insert(9);
     my_tree.insert(5);
     my_tree.insert(10);
@@ -21,6 +22,77 @@ TEST(BSTTest, test_insert_and_print) {
     std::string expected_string_repr =
         "9\n"
         "|--5\n"
+        "|  |--3\n"
+        "|  |  |--2\n"
+        "|  |  |--4\n"
+        "|  |--7\n"
+        "|     |--8\n"
+        "|--10\n"
+        "   |--12\n";
+    EXPECT_EQ(expected_string_repr, my_tree.to_string());
+}
+
+TEST(BSTTest, test_contains) {
+    ankur::binary_search_tree my_tree {};
+    my_tree.insert(9);
+    my_tree.insert(5);
+    my_tree.insert(10);
+    my_tree.insert(12);
+    my_tree.insert(7);
+    my_tree.insert(8);
+    my_tree.insert(3);
+    EXPECT_EQ(7, my_tree.size());
+    EXPECT_TRUE(my_tree.contains(8));
+    EXPECT_TRUE(my_tree.contains(9));
+    EXPECT_FALSE(my_tree.contains(11));
+    EXPECT_FALSE(my_tree.contains(1));
+}
+
+TEST(BSTTest, test_find_min_max) {
+    ankur::binary_search_tree my_tree {};
+    my_tree.insert(9);
+    my_tree.insert(5);
+    my_tree.insert(10);
+    my_tree.insert(12);
+    my_tree.insert(7);
+    my_tree.insert(8);
+    my_tree.insert(3);
+    EXPECT_EQ(12, my_tree.find_max());
+    EXPECT_EQ(3, my_tree.find_min());
+}
+
+TEST(BSTTest, test_remove) {
+    ankur::binary_search_tree my_tree {};
+    EXPECT_EQ(my_tree.size(), 0);
+    my_tree.insert(9);
+    my_tree.insert(5);
+    my_tree.insert(10);
+    my_tree.insert(12);
+    my_tree.insert(7);
+    my_tree.insert(8);
+    my_tree.insert(3);
+    my_tree.insert(2);
+    my_tree.insert(4);
+    my_tree.insert(6);
+    EXPECT_EQ(10, my_tree.size());
+    std::string expected_string_repr =
+        "9\n"
+        "|--5\n"
+        "|  |--3\n"
+        "|  |  |--2\n"
+        "|  |  |--4\n"
+        "|  |--7\n"
+        "|     |--6\n"
+        "|     |--8\n"
+        "|--10\n"
+        "   |--12\n";
+    EXPECT_EQ(expected_string_repr, my_tree.to_string());
+
+    my_tree.remove(5);
+    EXPECT_EQ(9, my_tree.size());
+    expected_string_repr =
+        "9\n"
+        "|--6\n"
         "|  |--3\n"
         "|  |  |--2\n"
         "|  |  |--4\n"
