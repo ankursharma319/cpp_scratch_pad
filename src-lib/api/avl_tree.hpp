8873e335b747bd4c4da5cf27dccfaaee740858bc
Ankur Sharma <ankurs4@cisco.com>
2022-05-08 22:43:31 +0200

    avl-tree implementation added with tests

--- src-lib/api/avl_tree.hpp
+++ src-lib/api/avl_tree.hpp
@@ -81,6 +81,84 @@ int node_height(node * n) {
     return n->height();
 }
 
+void left_rotate(node* x) {
+    assert(x);
+    node * y = x->right_child;
+    assert(y != nullptr);
+    node * b = y->left_child;
+
+    y->parent = x->parent;
+    if (x->parent && x->parent->value > x->value) {
+        assert(x->parent->left_child);
+        x->parent->left_child = y;
+    } else if(x->parent && x->parent->value < x->value) {
+        assert(x->parent->right_child);
+        x->parent->right_child = y;
+    }
+    x->parent = y;
+    y->left_child = x;
+    x->right_child = b;
+    if (b) {
+        b->parent = x;
+    }
+    x->update_heights();
+    y->update_heights();
+}
+
+void right_rotate(node* x) {
+    assert(x);
+    node * y = x->left_child;
+    assert(y != nullptr);
+    node * b = y->right_child;
+
+    y->parent = x->parent;
+    if (x->parent && x->parent->value > x->value) {
+        assert(x->parent->left_child);
+        x->parent->left_child = y;
+    } else if(x->parent && x->parent->value < x->value) {
+        assert(x->parent->right_child);
+        x->parent->right_child = y;
+    }
+    x->parent = y;
+    y->right_child = x;
+    x->left_child = b;
+    if (b) {
+        b->parent = x;
+    }
+    x->update_heights();
+    y->update_heights();
+}
+
+void _do_fix_avl_property(avl_detail::node* node) {
+    if (node->heaviness() > 1) {
+        if (node->right_child->heaviness() >= 0) {
+            left_rotate(node);
+        } else {
+            right_rotate(node->right_child);
+            left_rotate(node);
+        }
+    } else if (node->heaviness() < -1) {
+        if (node->left_child->heaviness() <= 0) {
+            right_rotate(node);
+        } else {
+            left_rotate(node->left_child);
+            right_rotate(node);
+        }
+    }
+    node->update_heights();
+}
+
+node * fix_avl_property(avl_detail::node* new_node) {
+    avl_detail::node * node = new_node;
+    while (true) {
+        _do_fix_avl_property(node);
+        if (!node->parent) {
+            return node;
+        }
+        node = node->parent;
+    }
+}
+
 }
 
 class avl_tree {
@@ -103,7 +181,7 @@ public:
         if (!inserted_node) {
             return;
         }
-        fix_avl_property();
+        m_head = fix_avl_property(inserted_node);
     }
     std::string to_string() const {
         if (!m_head) return "";
@@ -152,10 +230,6 @@ private:
             node = node->parent;
         }
         return inserted_node;
-    }
-
-    void fix_avl_property() {
-
     }
 
     avl_detail::node * m_head;
--- tests/AVLTests.cpp
+++ tests/AVLTests.cpp
@@ -14,8 +14,8 @@ TEST(AVLTest, test_simple_insert_and_print) {
     my_tree.insert(10);
     my_tree.insert(12);
     my_tree.insert(7);
-    my_tree.insert(8);
     my_tree.insert(3);
+    my_tree.insert(8);
     my_tree.insert(2);
     my_tree.insert(4);
     EXPECT_EQ(9, my_tree.size());
@@ -31,5 +31,54 @@ TEST(AVLTest, test_simple_insert_and_print) {
         "|     |--8(s=0,h=0)\n"
         "|--10(s=1,h=1)\n"
         "   |--12(s=0,h=0)\n";
+    EXPECT_EQ(expected_string_repr, my_tree.to_string());
+}
+
+TEST(AVLTest, test_avl_property_is_preserved_during_insert_right_rotate) {
+    ankur::avl_tree my_tree {};
+    my_tree.insert(9);
+    my_tree.insert(5);
+    my_tree.insert(4);
+    my_tree.insert(3);
+    EXPECT_EQ(4, my_tree.size());
+    std::string expected_string_repr =
+        "5(s=-1,h=2)\n"
+        "|--4(s=-1,h=1)\n"
+        "|  |--3(s=0,h=0)\n"
+        "|--9(s=0,h=0)\n";
+    EXPECT_EQ(expected_string_repr, my_tree.to_string());
+}
+
+TEST(AVLTest, test_avl_property_is_preserved_during_insert_left_rotate) {
+    ankur::avl_tree my_tree {};
+    my_tree.insert(3);
+    my_tree.insert(4);
+    my_tree.insert(5);
+    my_tree.insert(9);
+    EXPECT_EQ(4, my_tree.size());
+    std::string expected_string_repr =
+        "4(s=1,h=2)\n"
+        "|--3(s=0,h=0)\n"
+        "|--5(s=1,h=1)\n"
+        "   |--9(s=0,h=0)\n";
+    EXPECT_EQ(expected_string_repr, my_tree.to_string());
+}
+
+TEST(AVLTest, test_avl_property_is_preserved_during_zigzag_rotate) {
+    ankur::avl_tree my_tree {};
+    my_tree.insert(20);
+    my_tree.insert(14);
+    my_tree.insert(29);
+    my_tree.insert(10);
+    my_tree.insert(17);
+    my_tree.insert(16);
+    EXPECT_EQ(6, my_tree.size());
+    std::string expected_string_repr =
+        "17(s=0,h=2)\n"
+        "|--14(s=0,h=1)\n"
+        "|  |--10(s=0,h=0)\n"
+        "|  |--16(s=0,h=0)\n"
+        "|--20(s=1,h=1)\n"
+        "   |--29(s=0,h=0)\n";
     EXPECT_EQ(expected_string_repr, my_tree.to_string());
 }
