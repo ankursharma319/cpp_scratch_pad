7ad59246b7accf7e6eaa4e5afc1b7e5695f916ce
Ankur Sharma <ankurs4@cisco.com>
2022-05-21 12:30:49 +0200

    chained_hash_map: WIP1

--- src-lib/api/hash_map.hpp
+++ src-lib/api/hash_map.hpp
@@ -9,24 +9,59 @@
 #include <cassert>
 #include <iostream>
 #include <utility>
+#include <functional>
+#include "linked_list.hpp"
 
 namespace ankur {
+
+namespace chained_hash_map_detail {
+
+template<typename K, typename V>
+struct item {
+    K key;
+    V value;
+};
+
+}
 
 template<typename K, typename V>
 class chained_hash_map {
 
 public:
-    chained_hash_map() {};
+
+    using _arr_type = ankur::linked_list<chained_hash_map_detail::item<K, V>>;
+    chained_hash_map()
+    : m_array_capacity(8)
+    , m_array_occupied_size(0)
+    , m_array(new _arr_type[m_array_capacity])
+    {};
+
+    ~chained_hash_map() {
+        delete[] m_array;
+    }
 
     void insert(K const& key, V const& val) {
         (void) key;
         (void) val;
     }
 
+    void remove(K const& key) {
+        (void) key;
+    }
+
     bool contains(K const& key) const {
         (void) key;
         return false;
     }
+
+    std::size_t size() const {
+        return 0;
+    }
+
+private:
+    std::size_t m_array_capacity;
+    std::size_t m_array_occupied_size;
+    ankur::linked_list<chained_hash_map_detail::item<K, V>>* m_array {nullptr};
 };
 
 }
--- tests/HashMapTests.cpp
+++ tests/HashMapTests.cpp
@@ -11,3 +11,12 @@ TEST(ChainedHashMapTest, test_insert_and_contains) {
     EXPECT_EQ(true, map.contains(9));
     EXPECT_EQ(false, map.contains(99));
 }
+
+TEST(ChainedHashMapTest, test_size_and_remove) {
+    ankur::chained_hash_map<int, int> map {};
+    EXPECT_EQ(0, map.size());
+    map.insert(9, 99);
+    EXPECT_EQ(1, map.size());
+    map.remove(9);
+    EXPECT_EQ(0, map.size());
+}
