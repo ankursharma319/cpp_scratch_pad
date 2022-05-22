41eb9c154f98bbe2b9dbb0c80b732a4dbd98f80d
Ankur Sharma <ankurs4@cisco.com>
2022-05-22 23:31:29 +0200

    chained_hash_map: further cleanup, 1.5 to 2x slower than std::unordered_map

--- src-lib/api/hash_map.hpp
+++ src-lib/api/hash_map.hpp
@@ -1,28 +1,10 @@
 #ifndef ANKUR_HASH_MAP_HPP
 #define ANKUR_HASH_MAP_HPP
 
-#include <cstddef>
-#include <iterator>
-#include <memory>
-#include <sstream>
-#include <type_traits>
-#include <cassert>
-#include <iostream>
-#include <utility>
 #include <functional>
 #include <list>
 
 namespace ankur {
-
-namespace chained_hash_map_detail {
-
-template<typename K, typename V>
-struct item {
-    K key;
-    V value;
-};
-
-}
 
 template<typename K, typename V, typename Hash = std::hash<K>>
 class chained_hash_map {
@@ -32,7 +14,7 @@ public:
     chained_hash_map()
     : m_array_capacity(8)
     , m_array_occupied_size(0)
-    , m_array(new array_type[m_array_capacity]{0})
+    , m_array(new array_type[m_array_capacity]{})
     , m_pre_hash_fn()
     {};
 
@@ -42,19 +24,15 @@ public:
 
     bool insert(K const& key, V const& value) {
         std::size_t index = hash(key);
-        assert(index < m_array_capacity);
-        if (m_array[index].get() == nullptr) {
-            m_array[index] = std::make_unique<linked_list_type>();
-        }
-        auto it = m_array[index]->cbegin();
-        while(it != m_array[index]->cend()) {
+        auto it = m_array[index].cbegin();
+        while(it != m_array[index].cend()) {
             if (it->key == key) {
                 return false;
             }
             it++;
         }
-        chained_hash_map_detail::item<K, V> _item = {key, value};
-        m_array[index]->push_back(std::move(_item));
+        item _item = {key, value};
+        m_array[index].push_back(std::move(_item));
         m_array_occupied_size++;
         rehash_if_required();
         return true;
@@ -62,55 +40,41 @@ public:
 
     bool remove(K const& key) {
         std::size_t index = hash(key);
-        assert(index < m_array_capacity);
-        if (m_array[index].get() == nullptr) {
-            return false;
-        }
-        auto it = m_array[index]->cbegin();
-        while(it != m_array[index]->cend()) {
+        auto it = m_array[index].cbegin();
+        while(it != m_array[index].cend()) {
             if (it->key == key) {
                 break;
             }
             it++;
         }
-        if (it == m_array[index]->cend()) {
+        if (it == m_array[index].cend()) {
             return false;
         }
-        m_array[index]->erase(it);
+        m_array[index].erase(it);
         m_array_occupied_size--;
-        if (m_array[index]->empty()) {
-            m_array[index] = nullptr;
-        }
         rehash_if_required();
         return true;
     }
 
     V& operator[](K const& key) {
         std::size_t index = hash(key);
-        assert(index < m_array_capacity);
-        if (m_array[index].get() == nullptr) {
-            m_array[index] = std::make_unique<linked_list_type>();
-        }
-        auto it = m_array[index]->begin();
-        while(it != m_array[index]->end()) {
+        auto it = m_array[index].begin();
+        while(it != m_array[index].end()) {
             if (it->key == key) {
                 return it->value;
             }
             it++;
         }
-        chained_hash_map_detail::item<K, V> _item = {key, V()};
-        m_array[index]->push_back(std::move(_item));
+        item _item = {key, V()};
+        m_array[index].push_back(std::move(_item));
         m_array_occupied_size++;
-        return (--(m_array[index]->end()))->value;
+        rehash_if_required();
+        return (--(m_array[index].end()))->value;
     }
 
     bool contains(K const& key) const {
         std::size_t index = hash(key);
-        assert(index < m_array_capacity);
-        if (m_array[index].get() == nullptr) {
-            return false;
-        }
-        for(auto it = m_array[index]->cbegin(); it != m_array[index]->cend(); it++) {
+        for(auto it = m_array[index].cbegin(); it != m_array[index].cend(); it++) {
             if (it->key == key) {
                 return true;
             }
@@ -123,8 +87,14 @@ public:
     }
 
 private:
-    using linked_list_type = std::list<chained_hash_map_detail::item<K, V>>;
-    using array_type = std::unique_ptr<linked_list_type>;
+
+    struct item {
+        K key;
+        V value;
+    };
+
+    using linked_list_type = std::list<item>;
+    using array_type = linked_list_type;
 
     void rehash_if_required() {
         float current_factor = current_load_factor();
@@ -142,18 +112,12 @@ private:
     }
 
     void do_rehash(std::size_t target_capacity) {
-        array_type* new_array = new array_type[target_capacity] {0};
+        array_type* new_array = new array_type[target_capacity] {};
         // loop through all items in old array and move them to new array
         for (std::size_t i = 0; i < m_array_capacity; i++) {
-            if (m_array[i].get() == nullptr) {
-                continue;
-            }
-            for (auto it = m_array[i]->cbegin(); it != m_array[i]->cend(); it++) {
+            for (auto it = m_array[i].cbegin(); it != m_array[i].cend(); it++) {
                 std::size_t new_index = hash(it->key, target_capacity);
-                if (new_array[new_index].get() == nullptr) {
-                    new_array[new_index] = std::make_unique<linked_list_type>();
-                }
-                new_array[new_index]->push_back(*it);
+                new_array[new_index].push_back(*it);
             }
         }
         delete[] m_array;
