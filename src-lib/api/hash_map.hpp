475f78d74c60702a58d009eb2a46b4cae624ddba
Ankur Sharma <ankurs4@cisco.com>
2022-05-22 11:00:05 +0200

    chained_hash_map: insert and contains works

--- src-lib/api/hash_map.hpp
+++ src-lib/api/hash_map.hpp
@@ -24,44 +24,72 @@ struct item {
 
 }
 
-template<typename K, typename V>
+template<typename K, typename V, typename Hash = std::hash<K>>
 class chained_hash_map {
 
 public:
 
-    using _arr_type = ankur::linked_list<chained_hash_map_detail::item<K, V>>;
+    using _linked_list_type = ankur::linked_list<chained_hash_map_detail::item<K, V>>;
+    using _arr_type = std::unique_ptr<_linked_list_type>;
     chained_hash_map()
     : m_array_capacity(8)
     , m_array_occupied_size(0)
-    , m_array(new _arr_type[m_array_capacity])
+    , m_array(new _arr_type[m_array_capacity]{0})
+    , m_pre_hash_fn()
     {};
 
     ~chained_hash_map() {
         delete[] m_array;
     }
 
-    void insert(K const& key, V const& val) {
-        (void) key;
-        (void) val;
+    bool insert(K const& key, V const& value) {
+        std::size_t index = hash(key);
+        assert(index < m_array_capacity);
+        if (m_array[index].get() == nullptr) {
+            m_array[index] = std::make_unique<_linked_list_type>();
+        }
+        auto it = m_array[index]->cbefore_begin();
+        auto before_end_it = it;
+        while(it != m_array[index]->cend()) {
+            if ((*it).key == key) {
+                return false;
+            }
+            before_end_it = it++;
+        }
+        chained_hash_map_detail::item<K, V> _item = {key, value};
+        m_array[index]->insert_after(before_end_it, std::move(_item));
+        m_array_occupied_size++;
+        return true;
     }
 
     void remove(K const& key) {
         (void) key;
     }
 
-    bool contains(K const& key) const {
+    V& operator[](K const& key) {
         (void) key;
-        return false;
+        throw std::logic_error("Function not yet implemented");
+    }
+
+    bool contains(K const& key) const {
+        std::size_t index = hash(key);
+        assert(index < m_array_capacity);
+        return m_array[index].get() != nullptr;
     }
 
     std::size_t size() const {
-        return 0;
+        return m_array_occupied_size;
     }
 
 private:
+    std::size_t hash(K const& key) const {
+        return m_pre_hash_fn(key) % m_array_capacity;
+    }
+
     std::size_t m_array_capacity;
     std::size_t m_array_occupied_size;
-    ankur::linked_list<chained_hash_map_detail::item<K, V>>* m_array {nullptr};
+    _arr_type* m_array {nullptr};
+    Hash m_pre_hash_fn;
 };
 
 }
--- tests/HashMapTests.cpp
+++ tests/HashMapTests.cpp
@@ -7,7 +7,10 @@
 TEST(ChainedHashMapTest, test_insert_and_contains) {
     ankur::chained_hash_map<int, int> map {};
     EXPECT_EQ(false, map.contains(3));
-    map.insert(9, 99);
+    bool inserted = map.insert(9, 99);
+    EXPECT_TRUE(inserted);
+    inserted = map.insert(9, 88);
+    EXPECT_FALSE(inserted);
     EXPECT_EQ(true, map.contains(9));
     EXPECT_EQ(false, map.contains(99));
 }
@@ -19,4 +22,13 @@ TEST(ChainedHashMapTest, test_size_and_remove) {
     EXPECT_EQ(1, map.size());
     map.remove(9);
     EXPECT_EQ(0, map.size());
+}
+
+TEST(ChainedHashMapTest, test_value_access) {
+    ankur::chained_hash_map<int, int> map {};
+    map.insert(9, 99);
+    map.insert(9, 98);
+    map.insert(8, 88);
+    EXPECT_EQ(99, map[9]);
+    EXPECT_EQ(88, map[8]);
 }
