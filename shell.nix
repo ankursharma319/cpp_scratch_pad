18426e5e7b1b784ab258bcf0446eb7c082306238
Ankur Sharma <ankurs4@cisco.com>
2022-07-01 11:13:33 +0200

    nix: update nixpkgs sha

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7ac6901f5e3038d59d3e2576af445fe418291dd8.tar.gz") {}}:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9a17f325397d137ac4d219ecbd5c7f15154422f4.tar.gz") {}}:
 
 pkgs.mkShell {
     nativeBuildInputs = [
