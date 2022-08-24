139f12b05e7937c7011c90990434685620402502
Ankur Sharma <ankurs4@cisco.com>
2022-08-24 19:50:38 +0200

    algo_practice: is_palindrome_permutation bitvector version

--- src-lib/algo_practice.cpp
+++ src-lib/algo_practice.cpp
@@ -1,9 +1,10 @@
 #include "algo_practice.hpp"
 #include <unordered_map>
+#include <cassert>
 
 namespace {
 
-bool _is_palindrome_permutation_v1(std::string const& s) {
+[[maybe_unused]] bool _is_palindrome_permutation_v1(std::string const& s) {
     // standardize case, remove all non a-z chars
     // std::unordered_map populated character counts
     // if all but one of counts is even, then its a palindrome
@@ -37,12 +38,37 @@ bool _is_palindrome_permutation_v1(std::string const& s) {
     return true;
 }
 
+bool check_exactly_one_bit_set(std::uint32_t bit_vector) {
+    bool anded = (bit_vector - 1) & bit_vector;
+    return anded == 0;
+}
+
+[[maybe_unused]] bool _is_palindrome_permutation_v2(std::string const& s) {
+    std::uint32_t is_odd_bit_vector = 0U;
+    for (char c: s) {
+        unsigned short char_index;
+        if (c >= 'a' && c <= 'z') {
+            char_index = c - 'a';
+        } else if(c >= 'A' && c <= 'Z') {
+            char_index = c - 'A';
+        } else {
+            continue;
+        }
+        std::uint32_t mask = 1U << char_index;
+        is_odd_bit_vector = is_odd_bit_vector ^ mask;
+    }
+    if(is_odd_bit_vector == 0) {
+        return true;
+    }
+    return (is_odd_bit_vector == 0) ||  check_exactly_one_bit_set(is_odd_bit_vector);
+}
+
 }
 
 namespace ankur {
 
 bool is_palindrome_permutation(std::string const& s) {
-    return _is_palindrome_permutation_v1(s);
+    return _is_palindrome_permutation_v2(s);
 }
 
 }
